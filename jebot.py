19f2a97c9cf8d99a9a842572bf43c706cec63381
charindithjaindu <75799963+charindithjaindu@users.noreply.github.com>
2021-03-19 14:58:53 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -165,7 +165,7 @@ async def send_audio(message: Message, info_dict, audio_file):
         get_file_extension_from_url(thumbnail_url)
     # info (s2tw)
     webpage_url = info_dict['webpage_url']
-    title = '@Music24x7SL'+s2tw(info_dict['title'])
+    title = '@Music24x7SL '+s2tw(info_dict['title'])
     caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
     duration = int(float(info_dict['duration']))
     performer = s2tw(info_dict['uploader'])
@@ -216,7 +216,7 @@ async def send_video(message: Message, info_dict, video_file):
         get_file_extension_from_url(thumbnail_url)
     # info (s2tw)
     webpage_url = info_dict['webpage_url']
-    title = '@Music24x7SL'+s2tw(info_dict['title'])
+    title = '@Music24x7SL '+s2tw(info_dict['title'])
     caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
     duration = int(float(info_dict['duration']))
     width, height = get_resolution(info_dict)
